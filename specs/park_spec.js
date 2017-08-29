2d9b1e6b68accefa10383ed2de55e3195ac6e242
Chris Hunter <orkem2@gmail.com>
2017-08-29 16:10:09 +0100

    added test to park to check empty enclosure

---
+++ specs/park_spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var Park = require('../park.js');
+
+describe("Jurrasic park enclosure test", function(){
+  beforeEach(function(){
+    park = new Park();
+  })
+
+  it("should start empty", function(){
+    assert.strictEqual(park.enclosure.size(), 0);
+  })
+})
